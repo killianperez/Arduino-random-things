d909690f8e9ab9d9de448e8e771de078540caa66
Killian P <kikihzhz@gmail.com>
2021-03-30 22:06:08 +0200

    Add file

---
+++ bliking-led.ino
@@ -0,0 +1,16 @@
+/*
+LICENSE MIT
+BY : KILLIAN PEREZ
+DATE: 2019, December 17th
+DESCRIPTION:
+   Display temperature from LM35DZ sensor as metric system
+*/
+
+void setup() {
+}
+void loop() {
+    digitalWrite(2, HIGH);
+    delay(100);
+    digitalWrite(2, LOW);
+    delay(100);
+}
